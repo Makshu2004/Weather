bbd43c4f347488afc2a505f8f453fbc0579fcb60
Makshu2004 <makshudalinadaf@gmail.com>
2026-06-01 23:51:15 +0530

    image

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -11,7 +11,7 @@ import SunnyIcon from '@mui/icons-material/Sunny';
 export default function InfoBox({info}){
   const INIT_URL="https://images.unsplash.com/photo-1565010431002-0c91cad04dd3?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2VhdGhlciUyMGltYWdlfGVufDB8fDB8fHww"
 
-  const HOT_URL=" https://images.unsplash.com/photo-1752122094097-a27239f964cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHdlYXRoZXIlMjBob3QlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D ";
+  const HOT_URL="https://images.unsplash.com/uploads/14121010130570e22bcdf/e1730efe?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   const COLD_URL="https://images.unsplash.com/photo-1640942824064-6209c10b9091?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2VhdGhlciUyMGNvbGQlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D";
   const RAIN_URL="https://images.unsplash.com/photo-1573482617210-6e98fd82f52d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzl8fHdlYXRoZXIlMjBSQUlOJTIwaW1hZ2V8ZW58MHx8MHx8fDA%3D";
   
